e7636968d0c45c536d0c0ff22352e83fc147c996
FrogePerson <129311808+FrogePerson@users.noreply.github.com>
2024-11-16 14:44:38 +0300

    New base of prog

--- TxtChanger/TxtChanger.cpp
+++ TxtChanger/TxtChanger.cpp
@@ -3,19 +3,24 @@
 
 void OpenFile(std::string linkToFile);
 
-void InputTxtInPattern(std::string txt, std::string pattern);
+void ChangeTxtInPattern(std::string txt, std::string pattern, std::string place);
 
 void ChangeTxtInPattern(std::string txt, std::string pattern);
 
 
 int main()
 {
-    std::string link;
+    std::string link, pattern, place;
 
     std::cout << "Input link to file\n";
     std::getline(std::cin, link);
 
-    
+    std::cout << "Input Pattern\n";
+    std::getline(std::cin, pattern);
+
+    std::cout << "Input Place or press enter if u want change all string\n";
+    std::getline(std::cin, place);
+
 }
 
 
